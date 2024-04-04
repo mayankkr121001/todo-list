3024beef6999c20253dadbdfbc08f3c2a98b3c93
mayankkr121001 <mayanksahu121001@gmail.com>
2024-04-04 15:58:37 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import Tasks from './components/Tasks'
 
 function App() {
+
+  const [taskValue, setTaskValue] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [edited, setEdited] = useState(false);
+ 
+
+  const getInput = (e) => {
+    setTaskValue(e.target.value);
+  }
+
+  const addItems = () => {
+
+    setTaskList([...taskList, taskValue]);
+
+    setTaskValue("");
+    setEdited(false);
+  }
+
+  const deleteTask = (id) => {
+    const updated = taskList.filter((item, index) => {
+      return index !== id;
+    })
+    setTaskList(updated)
+  }
+
+  const editTask = (id) => {
+    const updated = taskList.filter((item, index) => {
+      return index !== id;
+    })
+    setTaskList(updated)
+
+    setTaskValue(taskList[id])
+    setEdited(true)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" w-screen min-h-screen h-fit bg-slate-900 py-[110px]">
+
+      <div className='bg-white w-[500px] mx-auto text-center px-5 py-2 rounded tracking-wider'>
+        <h1 className='text-5xl font-semibold text-[#2b2b2b]'>TODO LIST</h1>
+
+        <div className='w-full h-[1px] my-5 bg-[#9d9d9d]'></div>
+
+        <div className='text-left'>
+
+          <input onChange={getInput} className='w-full outline-none border border-[#9d9d9d] px-2 py-2 rounded' type="text" placeholder='add item...' value={taskValue} />
+
+          {edited ? <button onClick={addItems} className='bg-[#2b2b2b] text-white px-3 py-1 my-2 rounded'>Update</button>:<button onClick={addItems} className='bg-[#2b2b2b] text-white px-3 py-1 my-2 rounded'>ADD</button>}
+
+        </div>
+
+        <div className='py-4'>
+          {taskList.map((value, index) => {
+            return (
+              <Tasks key={index} task={value} deleteTask={() => deleteTask(index)} editTask={() => editTask(index)}/>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+
+const Tasks = ({ task, deleteTask, editTask }) => {
+
+  const [isCompleted, setCompleted] = useState(false);
+
+  const onTaskClick = ()=>{
+      setCompleted(!isCompleted);
+    }
+  
+
+  return (
+    <div className='bg-gray-300 px-4  flex justify-between items-center border border-gray-400'>
+
+      {isCompleted ? <p onClick={onTaskClick} className='text-[#2b2b2b] line-through'>{task}</p>  : <p onClick={onTaskClick} className='text-[#2b2b2b]'>{task}</p>}
+
+      <div>
+
+        <button onClick={deleteTask} className='bg-white text-[#2b2b2b] px-3 py-1 my-2 ml-2 rounded'>Delete</button>
+
+        <button onClick={editTask} className='bg-white text-[#2b2b2b] px-3 py-1 my-2 ml-2 rounded'>Edit</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Tasks
